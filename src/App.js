d1e25a711ccffe9a45d219cc7c324ce687fc4595
DieuveilleSuthi <mdieuveillesuthi@gmail.com>
2023-09-02 19:09:41 +0200

    signin and signout

--- src/App.js
+++ src/App.js
@@ -6,14 +6,15 @@ import ImageLinkForm from './component/ImageLinkForm/ImageLinkForm';
 //import Clarifai from 'clarifai';
 import 'tachyons';
 import Rank from './component/Rank/Rank';
-
+import Signin from './component/signin/signin';
+import Register from './component/Register/Register';
 import FaceRecognetion from './component/FaceRecognetion.js/FaceRecognetion';
 
 /*const app = new Clarifai.App({
   apiKey: '94452b931d65435b981ce1e0c39e2244'
 })*/
 
-const returnClarifaiJSONRequestOption = (imageUrl) => {
+/*const returnClarifaiJSONRequestOption = (imageUrl) => {
   // Your PAT (Personal Access Token) can be found in the portal under Authentification
   const PAT = '94452b931d65435b981ce1e0c39e2244';
   // Specify the correct user_id/app_id pairings
@@ -50,7 +51,7 @@ const returnClarifaiJSONRequestOption = (imageUrl) => {
 
 return requestOptions;
 
-}
+}*/
 
 
 class App extends Component {
@@ -59,22 +60,24 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl: '',
+      route:'signin',
+      isSignin: false
     }
   }
 
   onInputChange = (event) => {
     this.setState({input: event.target.value})
   }
-  
+
   onSubmit = () => {
     this.setState({imageUrl: this.state.input});
-   // app.models.predict('face-detection', this.state.input)
-    fetch("https://api.clarifai.com/v2/models/" + "face-detection" + "/versions/" + "/outputs", returnClarifaiJSONRequestOption(this.state.input))
+  /* // app.models.predict('face-detection', this.state.input)
+    fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/versions/" + "/outputs", returnClarifaiJSONRequestOption(this.state.input))
       .then(response => response.json())
       .then(response => {
         console.log( response)})
       .catch(error => console.log('error', error))  
-        /*if (response) {
+        if (response) {
           fetch('http://localhost:3000/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
@@ -92,17 +95,40 @@ class App extends Component {
       })
       .catch(err => console.log(err));*/
   }
+
+  onRouteChange = (route) => {
+    if(route === 'signout'){
+      this.setState({isSignin: false});
+    } else if(route === 'home'){
+      this.setState({isSignin: true});
+    }
+
+    this.setState({route: route});
+  }
   render(){
+
+    const {isSignin, imageUrl, route} = this.state;
     return (
       <div className="App">
-          <Navigation />
-          <Logo />
-          <Rank />
-          <ImageLinkForm 
-          onInputChange = {this.onInputChange}
-          onSubmit = {this.onSubmit}
-          />
-          <FaceRecognetion imageUrl = {this.state.imageUrl}/>
+          <Navigation 
+          isSignin={isSignin}
+          onRouteChange = {this.onRouteChange}/>
+          {route === 'home' ? 
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm 
+            onInputChange = {this.onInputChange}
+            onSubmit = {this.onSubmit}
+            />
+            <FaceRecognetion imageUrl = {imageUrl}/>
+         </div> 
+        : (route === 'signin' ? 
+        <Signin  onRouteChange = {this.onRouteChange}/>:
+        <Register  onRouteChange = {this.onRouteChange} />
+        )
+          }
+       
       </div>
     );
   }
--- src/component/Navigation/Navigation.js
+++ src/component/Navigation/Navigation.js
@@ -1,11 +1,20 @@
 import React from "react";
 
-const Navigation = () => {
-    return(
-        <nav style={{display: 'flex', justifyContent:'flex-end'}}>
-            <p className="f3 dim black underline pa3 pointer">Sign Out</p>
-        </nav>
-    )
+const Navigation = ({onRouteChange, isSignin}) => {
 
+    if(isSignin){
+        return(
+            <nav style={{display: 'flex', justifyContent:'flex-end'}}>
+                <p className="f3 dim black underline pa3 pointer" onClick={() => onRouteChange('signin')}>Sign Out</p>
+            </nav>
+        )
+    }else {
+        return(
+            <nav style={{display: 'flex', justifyContent:'flex-end'}}>
+                     <p className="f3 dim black underline pa3 pointer" onClick={() => onRouteChange('signin')}>Sign in</p>
+                     <p className="f3 dim black underline pa3 pointer" onClick={() => onRouteChange('register')}>Register</p>
+            </nav>
+        )
+    }
 }
 export default Navigation;
